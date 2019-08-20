087f814adc30c222ab8245c8ad4c2f08be29f8d6
James Lee <jameswlee109@gmail.com>
2019-08-20 00:33:17 -0500

    blah

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -2,7 +2,7 @@ let questionNumber = 0;
 let score = 0;
 
 
-function startQuiz (){
+function startQuiz(){
   $('.start-quiz').on('click', '.startButton', function(event){
     $('.start-quiz').remove();
     $('.questionAndChoices').css('display', 'block');
